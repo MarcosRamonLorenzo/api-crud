5e2b667b6932c88f858231356050e6553c1eb30a
m <marcossram31@gmail.com>
2026-02-24 10:14:07 +0100

    API REST CRUD con MongoDB Atlas

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
  PORT: process.env.PORT || 3000,
- DB: process.env.MONGODB || 'mongodb://127.0.0.1:27017/SD',
+ DB: process.env.MONGODB || 'mongodb+srv://marcossram31_db_user:HeTb5mJCDvVN6m7e@cluster0.yqbecvf.mongodb.net/?appName=Cluster0',
  TOKEN: 'password1234'
 }
